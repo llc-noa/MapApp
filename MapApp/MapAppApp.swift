50211263645838f7ab00682f24fd05f105687256
rsugaya <rsugaya@llc-noa.net>
2023-03-15 19:43:47 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by 菅谷亮太 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
